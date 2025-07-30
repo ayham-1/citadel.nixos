aded833b9363128a2c8e20615a0a75c4135efd10
ayham-1 <me@ayham.xyz>
2025-07-30 11:41:54 +0200

    add trailing cursor

--- users/ayham/progs/kitty.nix
+++ users/ayham/progs/kitty.nix
@@ -8,6 +8,12 @@
         scrollback_lines = 10000;
         enable_audio_bell = false;
         update_check_interval = 0;
+        cursor_trail = 3;
+        cursor_trail_decay = "0.1 0.4";
+        cursor_trail_start_threshold = 2;
+        cursor_shape = "block";
+        cursor_blink_interval = "-1";
+        cursor_stop_blinking_after = "15.0";
       };
       font.size = lib.mkDefault 10;
       shellIntegration.enableBashIntegration = true;
