40053bf1638693236582234c541e8ded22373895
ayham-1 <me@ayham.xyz>
2025-08-20 14:38:35 +0200

    update

--- profiles/development.nix
+++ profiles/development.nix
@@ -3,5 +3,5 @@
   pkgs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [gcc gnumake clang gdb stm32flash stm32loader stm32cubemx zig git-lfs];
+  environment.systemPackages = with pkgs; [gcc gnumake cmake clang gdb stm32flash stm32loader stm32cubemx zig git-lfs];
 }
