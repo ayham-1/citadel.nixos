70bdf6ab74e46ab4d2f9769ae61f9b3c6acc48e5
ayham-1 <me@ayham.xyz>
2025-09-23 08:02:55 +0200

    update

--- services/virt.nix
+++ services/virt.nix
@@ -1,7 +1,17 @@
-{ config, pkgs, lib, ... }: {
-  virtualisation.libvirtd = { enable = true; };
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  virtualisation.libvirtd = {enable = true;};
   virtualisation.podman.enable = true;
-  virtualisation.podman.dockerCompat = true;
+  virtualisation.docker.enable = true;
+  virtualisation.docker.storageDriver = "btrfs";
+  virtualisation.docker.rootless = {
+    enable = true;
+    setSocketVariable = true;
+  };
 
   environment.systemPackages = with pkgs; [
     virt-manager
